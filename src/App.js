8a2ba0f90cf6b7b511152d2cb76844bef9a07c78
Sudarshan Chippa <suddu.ch@gmail.com>
2022-02-09 22:22:28 +0530

    updated info, added header footer along with whats up integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { useState, Component } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookOpenReader } from '@fortawesome/free-solid-svg-icons';
+import FloatingWhatsApp from 'react-floating-whatsapp';
 import './App.css';
 
 const App = () => {
-    const [appName, setAppName] = useState("Sudarshan's Code App");
+    const [appName, setAppName] = useState("  Sudarshan's Tech Journal");
     return (
         <div className='App'>
-            <h1>{appName}</h1>
+            <div id="header">
+                <h1>
+                    <span className='readerBook'>
+                        <FontAwesomeIcon icon={faBookOpenReader} />
+                    </span>
+                    {appName}
+                </h1>
+            </div>
+            <div id="content"></div>
+            <div id="footer">
+                <div>Contact for any UI/UX Frontend realted help in React, Angular or Java/ Sprint boot / Python</div>
+                <div>contact: suddu.ch@gmail.com</div>
+                <FloatingWhatsApp 
+                    phoneNumber="919989696056"  
+                    accountName="Sudarshan"
+                    chatMessage="Hi Sudarshan, I saw your tech journal profile, need your help in following tech stack"
+                    allowClickAway
+                    notification
+                    notificationDelay={60000} // 1 minute
+                    notificationSound
+                />
+            </div>
         </div>
     )
 }
